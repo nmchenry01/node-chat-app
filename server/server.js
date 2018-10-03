9dd07494d2e58d162a310534d27bdfd7483d76c9
nmchenry01 <nicholas.mchenry@levvel.io>
2018-10-03 12:50:34 -0500

    Added Socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const PORT = 3000
 
 const app = express();
+const server = http.createServer((app));
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New User Connected');
+
+    socket.on('disconnect', () => {
+        console.log('User has Disconnected')
+    })
+})
+
+server.listen(PORT, () => {
     console.log(`Server is listening on Port ${PORT}`)
 })
