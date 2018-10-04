b4ad15e6ae1a64ed165bdbd3d65d0974a6922c08
nmchenry01 <nicholas.mchenry@levvel.io>
2018-10-04 12:36:54 -0500

    Added UI/UX improvements

--- public/js/index.js
+++ public/js/index.js
@@ -16,28 +16,30 @@ socket.on("newMessage", function(message) {
   $("#messages").append(li);
 });
 
-socket.on('newLocationMessage', function (message) {
-    console.log("newLocationMessage", message);
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My Current Location</a>');
+socket.on("newLocationMessage", function(message) {
+  console.log("newLocationMessage", message);
+  var li = $("<li></li>");
+  var a = $('<a target="_blank">My Current Location</a>');
 
-    li.text(`${message.from}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+  li.text(`${message.from}: `);
+  a.attr("href", message.url);
+  li.append(a);
+  $("#messages").append(li);
 });
 
 $("#message-form").on("submit", function(e) {
   e.preventDefault();
 
+  var messageTextbox = $("[name=message]");
+
   socket.emit(
     "createMessage",
     {
       from: "User",
-      text: $("[name=message]").val()
+      text: messageTextbox.val()
     },
     function(data) {
-      console.log(data);
+      messageTextbox.val("");
     }
   );
 });
@@ -49,14 +51,17 @@ locationButton.on("click", function() {
     return alert("Your browser does not support Geolocation.");
   }
 
+  locationButton.attr("disabled", true).text('Sending location...');
   navigator.geolocation.getCurrentPosition(
     function(position) {
+      locationButton.attr("disabled", false).text('Send location');
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     function() {
+      locationButton.attr("disabled", false).text('Send location');
       alert("Unable to fetch location");
     }
   );
